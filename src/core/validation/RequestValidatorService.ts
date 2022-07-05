9e87b632768b70180f212ed0c6212cbda72274c6
Sixto Ortega <ortegasixto7@gmail.com>
2022-07-05 09:34:44 -0400

    Adding core validations

---
+++ src/core/validation/RequestValidatorService.ts
@@ -0,0 +1,44 @@
+import { BadRequestException } from './exceptions/BadRequestException';
+import { NotFoundException } from './exceptions/NotFoundException';
+import { Response } from 'express';
+
+export class RequestValidatorService {
+  // async verifyToken(token: string | undefined): Promise<string> {
+  //   if (!token) throw new BadRequestException(ExceptionCodeEnum.INVALID_AUTH_TOKEN);
+  //   const result = await this.authService.verifyToken(token);
+  //   if (!result) throw new BadRequestException(ExceptionCodeEnum.INVALID_AUTH_TOKEN);
+  //   return result.uid;
+  // }
+
+  // async verifyTokenAndRole(token: string | undefined, roles: AuthUserRoleEnum[]): Promise<string> {
+  //   if (!token) throw new BadRequestException(ExceptionCodeEnum.INVALID_AUTH_TOKEN);
+  //   const result = await this.authService.verifyToken(token);
+  //   if (!result) throw new BadRequestException(ExceptionCodeEnum.INVALID_AUTH_TOKEN);
+
+  //   if (roles) {
+  //     if (!roles.find((role) => role === result.role)) {
+  //       throw new BadRequestException(ExceptionCodeEnum.UNAUTHORIZED);
+  //     }
+  //   }
+
+  //   return result.uid;
+  // }
+
+  async responseWrapper(callbackFunction: Function, res: Response): Promise<any> {
+    try {
+      const response = await callbackFunction();
+      res.status(200).json(response ? { data: response } : undefined);
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        console.warn(`BAD_REQUEST ${error.message}`);
+        res.status(400).json({ errorCode: error.message });
+      } else if (error instanceof NotFoundException) {
+        console.warn(`NOT_FOUND ${error.message}`);
+        res.status(404).json({ errorCode: error.message });
+      } else {
+        console.error(error);
+        res.status(500).json({ errorCode: 'INTERNAL_ERROR' });
+      }
+    }
+  }
+}
