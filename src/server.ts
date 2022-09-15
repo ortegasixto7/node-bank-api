8a09c32164726eb6d3075754c48c0ff427dbbb7f
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-15 10:53:44 -0400

    Refactoring database initialization

--- src/server.ts
+++ src/server.ts
@@ -5,40 +5,42 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { PORT } from './config/config';
 import { MongoDbClient } from './persistence/mongoDb/MongoDbClient';
-import { userRouter } from './presentation/userRouter';
-import { operationRouter } from './presentation/operationRouter';
-import { currencyRouter } from './presentation/currencyRouter';
-import { accountRouter } from './presentation/accountRouter';
-import { cardRouter } from './presentation/cardRouter';
 
-MongoDbClient.getInstance().catch((err) => console.error(err));
+MongoDbClient.initDb()
+  .then(async () => {
+    const { userRouter } = await import('./presentation/userRouter');
+    const { operationRouter } = await import('./presentation/operationRouter');
+    const { currencyRouter } = await import('./presentation/currencyRouter');
+    const { accountRouter } = await import('./presentation/accountRouter');
+    const { cardRouter } = await import('./presentation/cardRouter');
+    const app = express();
+    app.use(morgan('dev'));
+    app.use(express.json({ limit: '20mb' }));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(helmet());
+    app.use(compression());
+    app.use(cors());
 
-const app = express();
-app.use(morgan('dev'));
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(compression());
-app.use(cors());
+    app.use(function (req, _, next) {
+      console.info(`REQUEST_BODY: ${JSON.stringify(req.body)}`);
+      next();
+    });
 
-app.use(function (req, _, next) {
-  console.info(`REQUEST_BODY: ${JSON.stringify(req.body)}`);
-  next();
-});
+    const router = Router();
 
-const router = Router();
+    router.get('/', (_, res: Response) => {
+      res.status(200).json({ msg: 'API is working' });
+    });
 
-router.get('/', (_, res: Response) => {
-  res.status(200).json({ msg: 'API is working' });
-});
+    router.use('/users', userRouter);
+    router.use('/operations', operationRouter);
+    router.use('/currencies', currencyRouter);
+    router.use('/accounts', accountRouter);
+    router.use('/cards', cardRouter);
+    app.use(router);
 
-router.use('/users', userRouter);
-router.use('/operations', operationRouter);
-router.use('/currencies', currencyRouter);
-router.use('/accounts', accountRouter);
-router.use('/cards', cardRouter);
-
-app.use(router);
-app.listen(PORT, () => {
-  console.log(`-------\n App listening on port => ${PORT}\n-------`);
-});
+    app.listen(PORT, () => {
+      console.log(`-------\n App listening on port => ${PORT}\n-------`);
+    });
+  })
+  .catch((err) => console.error(err));
